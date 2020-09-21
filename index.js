652dcf91ee703ceefc1b3a4ca0a77accb6d04bdc
edensongjbs <edensongjbs@gmail.com>
2020-09-21 14:50:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function bfs(rootNode, vertices, edges){
 
 }
+
+function otherVertex(node, edge) {
+    return edge[0]===node ? edge[1] : edge[0]
+} 
+
+function findAdjacent(rootNode, vertices, edges) {
+    let vertexName, adjacents = []
+    edges.forEach(edge => {
+        if (edge.includes(rootNode)) {
+            vertexName = otherVertex(rootNode, edge)
+            adjacents.push(vertices.find(vert => vert.name===vertexName))
+        }
+    })
+    console.log(adjacents)
+    return adjacents.filter(adj => adj.distance===null)
+}
